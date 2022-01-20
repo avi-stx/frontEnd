d4e0081407e5720147e749f14ebe9acb9dd498af
tal-seagate <tal.alfi@seagate.com>
2022-01-20 10:33:05 +0000

    adding file name and extension to the table

--- micro-server/src/Components/tableComponents/TableBody.js
+++ micro-server/src/Components/tableComponents/TableBody.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
+const getFileNAme = (file)=> {
+    return file.split('.')[0]
+}
+
+const getFileExtension = (file)=> {
+    return file.split('.')[1]
+}
+
+const getFileDescription = (file) =>{
+    return [getFileNAme(file),getFileExtension(file)]
+}
+
 export default function TableBody(props) {
     console.log("in TableBody with props: ",props)
     const rows = props.filesList.map(element => {
-        let i=0;
+
+        const [fileName, fileExtension] = getFileDescription(element.name)
         return(
             <tr key={element.id}> 
-                <td>{element.name}</td>
-                <td>{element.extension}</td>
+                <td>{fileName}</td>
+                <td>{fileExtension}</td>
                 <td>{element.size}</td>
                 <td>{Date(element.date)}</td>
                 <td><button>Delete</button></td>
