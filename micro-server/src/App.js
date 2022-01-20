e5ff73887581259defcb6a81e23be493e4719cff
tal-seagate <tal.alfi@seagate.com>
2022-01-20 13:54:22 +0000

    chaning header style

--- micro-server/src/App.js
+++ micro-server/src/App.js
@@ -5,7 +5,7 @@ import FilesTable from './Components/tableComponents/FilesTable';
 function App() {
   return (
     <div className="App">
-      <header>Welcome to Micro-Server by Tal Alfi</header>
+      <header className='App-header'>Micro-Server by Tal Alfi</header>
       {/* <Upload /> */}
         <FilesTable />
     </div>
