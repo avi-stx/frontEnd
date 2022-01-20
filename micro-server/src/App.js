06c142d218b138b04eed02c46e5a1e3f5dc17256
tal-seagate <tal.alfi@seagate.com>
2022-01-20 10:23:11 +0000

    moving logic to other file - TableFiles.js

--- micro-server/src/App.js
+++ micro-server/src/App.js
@@ -1,14 +1,5 @@
 import './App.css';
 import FilesTable from './Components/tableComponents/FilesTable';
-import axios from 'axios';
-
-
-async function fetchData(){
-  console.log("sending request")
-  const response =
-    await axios.get("http://localhost:8080/files")
-  console.log(response.data)
-}
 
 
 function App() {
@@ -16,7 +7,6 @@ function App() {
     <div className="App">
       <header>Welcome to Micro-Server by Tal Alfi</header>
       {/* <Upload /> */}
-      <button onClick={fetchData} >get data from backend</button>
         <FilesTable />
     </div>
   );
