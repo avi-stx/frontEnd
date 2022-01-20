f330170e461e7f05fe79038508318f77047e13f5
tal-seagate <tal.alfi@seagate.com>
2022-01-20 10:32:46 +0000

    removing blank lines

--- micro-server/src/Components/tableComponents/FilesTable.js
+++ micro-server/src/Components/tableComponents/FilesTable.js
@@ -1,19 +1,13 @@
 import TableHeader from './TableHeader';
 import './FilesTable.css'
 import TableBody from './TableBody';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-
-
 
 
 const FilesTable = () => 
 {
-
     const [tableStruct,setTableStruct] = useState(false)
-
     async function fetchData(){
-
         fetch("http://localhost:8080/files").then((response) => {
             return response.json();
           }) .then((data) => {
@@ -23,7 +17,6 @@ const FilesTable = () =>
               };
             });
             setTableStruct(transformedTable);
-    
           });
       }
 
@@ -33,11 +26,8 @@ const FilesTable = () =>
         )()
       }, [])
 
-
-    
     return (
         <>
-            <button onClick={fetchData} >get data from backend</button>
             <table className="styled-table">
                 <TableHeader />
                 {tableStruct && <TableBody filesList={tableStruct}/>}
